a9c05c6707ad883f0084a2ca9d4f3eacf2879c1a
sahasra <sahasrareddy950@gmail.com>
2025-09-27 20:29:15 +0530

    updatedfile

--- daay-5.py
+++ daay-5.py
@@ -1,6 +1,3 @@
-x=15
-y=20
-print("x>y:",x>y)
-print("x<y:",x<y)
-print("x==y:",x==y)
-print("x!=y:",x!=y)
+print("True and False:", True and False)
+print("True or False:", True or False)
+print("not True:", not True)
