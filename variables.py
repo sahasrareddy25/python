9525a65f17de024acaa7a51e99b85952f0493ed3
sahasra <sahasrareddy950@gmail.com>
2025-09-28 21:44:16 +0530

    addedfile

---
+++ variables.py
@@ -0,0 +1,7 @@
+#variable basics
+#assign name,age,percentage marks to three different variables.print them using f-string
+
+name="sahasra"
+age="20"
+marks="89.5"
+print(f" My name is {name}, I am {20} years old, and I sccored {89.5}% marks.")
