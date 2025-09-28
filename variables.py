2feae22fb0f2a8fe3cb432acd96de9eb416d6389
sahasra <sahasrareddy950@gmail.com>
2025-09-28 21:54:52 +0530

    addedfile

--- variables.py
+++ variables.py
@@ -1,7 +1,11 @@
 #variable basics
 #assign name,age,percentage marks to three different variables.print them using f-string
-
 name="sahasra"
 age="20"
 marks="89.5"
 print(f" My name is {name}, I am {20} years old, and I sccored {89.5}% marks.")
+
+#multiple assignments
+#Assign values 10,20,30 to three variables a ,b,c in a single line.print the sum
+a,b,c=10,20,30
+print("sum =",a+b+c)
