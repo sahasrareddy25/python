faae536480ac70bcd47f77abec1832b84d7c36a9
sahasra <sahasrareddy950@gmail.com>
2025-10-01 09:31:20 +0530

    addedfile

---
+++ input.py
@@ -0,0 +1,22 @@
+"""#Svariable
+s="sahasra"
+print(s)"""
+
+#MVariables
+M="sahasra"
+age=20
+city="warangal"
+print(M,age,city)
+
+"""#3inputs
+x,y,z=input("Enter three values:").split()
+print("Total number of students:",x)
+print("Number of boys is:",y)
+print("Number of girls is:",z)
+
+#CU input
+age=int(input("Enter your age:"))
+if age>=18:
+ print("Eligible")
+else:
+ print("Not Eligible")"""
