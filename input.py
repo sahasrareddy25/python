d286218c1ca2bbdda21cd133f179b1efb3ba0bb5
sahasra <sahasrareddy950@gmail.com>
2025-10-01 09:40:25 +0530

    addedfile

--- input.py
+++ input.py
@@ -6,17 +6,27 @@ print(s)
 M="sahasra"
 age=20
 city="warangal"
-print(M,age,city)"""
+print(M,age,city)
 
-"""3inputs
+3inputs
 x,y,z=input("Enter three values:").split()
 print("Total number of students:",x)
 print("Number of boys is:",y)
-print("Number of girls is:",z)"""
+print("Number of girls is:",z)
 
 #CU input
 age=int(input("Enter your age:"))
 if age>=18:
  print("Eligible")
 else:
- print("Not Eligible")
+ print("Not Eligible")"""
+
+marks= int(input("Enter your marks:"))
+if marks>=100:
+ print("Grade A")
+elif marks>= 75:
+ print("Grade B")
+elif marks>= 60:
+ print("Grade C")
+else:
+ print("Grade D")
