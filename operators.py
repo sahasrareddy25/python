642bc042bddc0d414dacca04ad959519f5d1777d
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-10-03 23:31:45 +0530

    Addedfile

--- operators.py
+++ operators.py
@@ -7,18 +7,25 @@ print("a*b",a*b)
 print("a/b",a/b)
 print("a//b",a//b)
 print("a%b",a%b)
-print("a**b",a**b)"""
+print("a**b",a**b)
 
-"""#calculating intrest   # based on formula
+#calculating intrest   # based on formula
 p=10000
 r=2
 t=5
 intrest=(p*r*t)/100
-print("intrest:",intrest)"""
+print("intrest:",intrest)
 
 #shopping price
 price=5000
 quantity=10
 discount=10
 totalprice=price*quantity-discount
-print("totalprice:",totalprice)
+print("totalprice:",totalprice)"""
+
+#L.operators
+age=int(input("Enter your age:"))
+if age>=18:
+ print("Eligible to vote")
+else:
+ print("Not eligible")
