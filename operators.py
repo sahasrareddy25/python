ce8e0faa114ca959761bf9771be9ab4ce2603c69
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-10-03 23:23:41 +0530

    addedfile

--- operators.py
+++ operators.py
@@ -9,9 +9,16 @@ print("a//b",a//b)
 print("a%b",a%b)
 print("a**b",a**b)"""
 
-#calculating intrest   # based on formula
+"""#calculating intrest   # based on formula
 p=10000
 r=2
 t=5
 intrest=(p*r*t)/100
-print("intrest:",intrest)
+print("intrest:",intrest)"""
+
+#shopping price
+price=5000
+quantity=10
+discount=10
+totalprice=price*quantity-discount
+print("totalprice:",totalprice)
