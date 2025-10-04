725aac3f577bf821f19420b68c530a76d2661c5f
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-10-04 21:13:58 +0530

    addedfile

--- operators.py
+++ operators.py
@@ -21,12 +21,24 @@ price=5000
 quantity=10
 discount=10
 totalprice=price*quantity-discount
-print("totalprice:",totalprice)"""
+print("totalprice:",totalprice)
 
 #L.operators
 age=int(input("Enter your age:"))
 if age>=18:
  print("Eligible to vote")
 else:
- print("Not eligible")
+ print("Not eligible")"""
+
+
+#c.operators
+a=10
+b=20
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+
 
