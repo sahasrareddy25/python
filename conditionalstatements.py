cabbdd48d3d94d9aac68c680edb296a0a1f10425
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-10-07 17:54:21 +0530

    addedfile

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -1,8 +1,12 @@
 """#if condition 
 x=10
 if x>5:
- print("x is greater than 5")"""
+ print("x is greater than 5")
 
 x=20
 if x<2:
-  print("x is less than 2")
+  print("x is less than 2")"""
+
+age=20
+if age>=18:
+ print("Eligible to vote")
