650bc8c80236bd34afe042ce87aff3da066c8c6f
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-10-19 22:47:40 +0530

    Addedfile

--- loops.py
+++ loops.py
@@ -20,10 +20,13 @@ for i in range(5, 0, -1):                  #here start at 5 and stop before 0
 
 #Print numbers from 1 to 10 using a for loop.
 for numbers in range(1,11):
-    print(numbers)"""
+    print(numbers)
 
 #Print your name 5 times using a loop.
 for i in range(5):
-    print("sahasra")
+    print("sahasra")"""
 
+#Print numbers in reverse order from 10 to 1.
+for i in range(10,0,-1):
+    print(i)
 
