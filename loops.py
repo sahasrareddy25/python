9dc7b67abb84f2a3606fb485351aee91e490e160
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-10-19 22:28:55 +0530

    Addedfile

--- loops.py
+++ loops.py
@@ -1,9 +1,15 @@
 """for i in range(5, 6):
-    print(i)"""
+    print(i)
 
 for i in range(1, 5):
     print("Hello")
 
-"""#counting apples
+#counting apples
+
 for apple in range(1, 6):
     print("This is apple no.s ", apple)"""
+
+#printing names
+for i in range(3):
+    print("Sahasra")
+
