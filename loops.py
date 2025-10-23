c5567e43d5179d9a450761d1ea0e520a37d59176
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-10-23 22:16:41 +0530

    Addedfile

--- loops.py
+++ loops.py
@@ -13,20 +13,30 @@ for apple in range(1, 6):
 for i in range(3):
     print("Sahasra")"""
 
-#counting before before
+"""#counting before before
 for i in range(5, 0, -1):                  #here start at 5 and stop before 0
     print("Starting in", i)
 
 
 #Print numbers from 1 to 10 using a for loop.
 for numbers in range(1,11):
-    print(numbers)
+    print(numbers)"""
 
 """#Print your name 5 times using a loop.
 for i in range(5):
     print("sahasra")"""
 
-#Print numbers in reverse order from 10 to 1.
+"""#Print numbers in reverse order from 10 to 1.
 for i in range(10,0,-1):
-    print(i)
+    print(i)"""
+
+#leet code prblm- move zeroes
+class Solution(object):
+    def moveZeroes(self, nums):
+        j = 0 
+        for i in range(len(nums)):
+            if nums[i] != 0: 
+                nums[i], nums[j] = nums[j], nums[i]
+                j += 1 
+
 
