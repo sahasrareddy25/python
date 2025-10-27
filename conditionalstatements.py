71f4e38d5ae7807d93b4385d9d4834977d5eeba2
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-10-27 23:05:48 +0530

    Addedfile

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -286,5 +286,27 @@ elif age<=64:
 elif age<100:
     print("ticket price is $15")
 
+#leetcode
+class Solution(object):
+    def isPerfectSquare(self, num):
+        if num < 2:
+            return True
+
+        left, right = 2, num // 2
+        while left <= right:
+            mid = (left + right) // 2
+            squared = mid * mid
+            if squared == num:
+                return True
+            elif squared < num:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return False
+        
+
+
+
 
 
