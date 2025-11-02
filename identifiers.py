bcc487d6acf8da8ffc4b725182f305fd72eaa000
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-11-02 12:22:44 +0530

    Addedfile

---
+++ identifiers.py
@@ -0,0 +1,7 @@
+#x is a identifier
+#name is a identifer
+#greet is a identifier in the below code 
+x=10
+name="sahasra"
+def greet():
+    print("helloo")
---
+++ keywords.py
@@ -0,0 +1,7 @@
+#by using below code we can identify the keywords in python
+import keyword
+print(keyword.kwlist)
+
+#2-11-2025
+if x>10:
+    print("greater")
