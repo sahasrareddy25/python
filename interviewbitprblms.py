ffc8e1cd1852ae1487f8fd58184b9b6156a4688e
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-11-03 23:53:14 +0530

    Addedfile

--- interviewbitprblms.py
+++ interviewbitprblms.py
@@ -1,7 +1,21 @@
-#input and output6
+"""#input and output6
 def main():
     x = input()
     print("Hello",x)
 
 if __name__ =='__main__':
+    main()"""
+
+#variables and types
+def main():
+    my_string1 = "InterviewBit"
+    my_string2 = "Don't change the variable name"
+    my_intstring3 = "11"
+    my_float4 = "20.000000"
+
+    print("my_string1 =", my_string1)
+    print("my_string2 =", my_string2)
+    print("my_string3 =", my_string3)
+
+if __name__ == "__main__":
     main()
