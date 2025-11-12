86934c0f662b6aae17d581b47d67e421bc128c41
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-11-12 22:42:56 +0530

    Addedfile

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -1,9 +1,9 @@
-#if condition 
+"""#if condition 
 x=10
 if x>5:
  print("x is greater than 5")
 
-"""x=22
+x=22
 if x<2:
   print("x is less than 2")"""
 
@@ -303,7 +303,7 @@ class Solution(object):
             else:
                 right=mid - 1
 
-        return False"""
+        return False
     
 #leetcode-palindrome
 class Solution(object):
@@ -316,7 +316,26 @@ class Solution(object):
             return True
         else:
             return False
-        
+
+#check even or odd 
+num = int(input("Enter the number: "))
+if num % 2 == 0:
+    print("Even")
+else:
+    print("Odd")"""
+
+#find the largest of two numbers
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+if a > b:
+    print("A is greater")
+elif b > a:
+    print("B is greater")
+else:
+    print("Both are equal")
+
+
+
 
 
 
