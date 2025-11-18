e6cfbf7ff738ec785a1193e6c03ead7fe7c51cbc
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-11-18 23:37:43 +0530

    Addedfile

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -409,9 +409,9 @@ b = float(input("Enter second number: "))"""
 #check even or odd
 num=int(input("Enter the number: "))
 if num % 4 == 0:
-    print("number is odd.")
+    print("number is odd")
 else:
-    print("number is even.")
+    print("number is even")
 
 
 
