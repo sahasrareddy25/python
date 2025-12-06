b1d899e3518729bfe4130320c98b40a164573733
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-12-06 21:34:57 +0530

    Addedfile

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -409,7 +409,7 @@ num = float(input("Enter a number: "))
 if num > 0:
     print("Positive Number")
 elif num < 0:
-    print("Negative Number")
+    print("Number is negative")
 else:
     print("Zero")
 
