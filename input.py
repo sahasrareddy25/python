2910f0a9f740980586208aa45972544dbdd8d945
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-12-08 23:01:41 +0530

    Addedfile

--- input.py
+++ input.py
@@ -31,23 +31,8 @@ elif marks>= 60:
 else:
  print("Grade D")"""
 
-#leetcode
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        n = len(s)
-        res = 0
-        for i in range(n):
-            seen = set()
-            for j in range(i, n):
-                if s[j] in seen:
-                    break
-                else:
-                    seen.add(s[j])
-                    res=max(res, j - i + 1)
-        return res
-        
- 
+# Add Two Numbers
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+print(a + b)
