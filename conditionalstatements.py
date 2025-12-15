cb52b41488bff7e74e5ee564fb41d5ddcbb55b4d
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-12-15 11:27:29 +0530

    Addedfile

--- conditionalstatements.py
+++ conditionalstatements.py
@@ -5,13 +5,13 @@ if x>5:
 
 x=22
 if x<2:
-  print("x is less than 2")"""
+  print("x is less than 2")
 
-"""age=20
+age=20
 if age>=18:
- print("Eligible to vote")"""
+ print("Eligible to vote")
 
-"""age=16
+ age=16
 if age<18:
  print("are minors")
 
@@ -19,15 +19,7 @@ if age<18:
 if 0:
   print("")
 
-wish="helloo"
-print("helloo")
-
-a=5
-b=20
-if a<b:
- print("a is greater than b")"""
-
-"""total_marks=100
+total_marks=100
 pass_mark=35
 if total_marks>=pass_mark:
   print("student has passed")
@@ -40,18 +32,8 @@ phone_battery=100
 if phone_battery>20:
     print("phone battery is below")
 
-marks=100
-if marks >=90:
- print("grade: A")
-if marks >=70:
- print("grade: c")
-if marks>=50:
- print("grade: e")"""
- 
- 
-#10.10.2025
-
-"""#Positive or Negative
+ #10.10.2025
+#Positive or Negative
 if 5 > 0:
   print("number is positive")
 if -5 < 0:
@@ -68,43 +50,12 @@ if number % 7!=0:
 if number % 4==0:
  print("number is divisible by 4")
 
-
-#voting eligibility
-age=20
-if age>=18:
- print("eligible to vote")
-if age<18:
- print("not eligible to vote")
-if age==18:
- print("eiligible for first time voting")
-
-#largest of two numbers
-a=10
-b=20
-if a>b:
-  print("a is greater")
-if b>a:
- print("b is greater")
-if a==b:
- print("both are equal")
-
 #multiple of 5
 number=15
 if number % 5==0:
   print("number is multiple of 5")
 if number % 5!=0:
  print("number is not multiple of 5")  
-
-#grade calculation
-marks=85
-if marks>=90:
-  print("grade A")
-if marks>=75:
- print("grade B")
-if marks>=60:
- print("grade c")
-if marks<60:
- print("grade D")
 
 #smallest of three numbers 
 a=10
@@ -115,67 +66,10 @@ if a<b and a<c:
 if b<a and b<c:
  print("b is smallest")
 if c<a and c<b:
- print("c is smallest")"""
+ print("c is smallest")
 
 #11.10.2025
-
-"""#elif statements
-age=25
-if age<=12:
-  print("Child.")
-elif age<=19:
-  print("Teenager.")
-elif age<=35:
-  print("Young adult.")
-else:
-  print("Adult.")
-
-#marks & grades
-int_marks=int(input("Enter your marks: "))
-if int_marks>=90:
-  print("Grade A")
-elif int_marks>=75:
-  print("Grade B")
-elif int_marks>=50:
-  print("Grade C")
-else:
-  int_marks<50
-  print("Garde D")
-
-"""
-
-"""#enter a number from 1 to 7
-
-day = int(input("Enter a number (1-7): "))
-if day == 1:
-  print("Monday")
-elif day == 2:
-  print("Tuesday")
-elif day == 3:
-  print("Wednesday")
-elif day == 4:
-  print("Thursday")
-elif day == 5:
-  print("Friday")
-elif day == 6:
-  print("Saturday")
-elif day == 7:
-  print("Sunday")
-else:
-  print("Invalid input!")"""
-
-"""#Find the largest number
-a=int(input("Enter first number: "))
-b=int(input("Enter second number:"))
-c=int(input("Enter third number: "))
-if a>=b and a>=c:
-    print(a is largest number)
-elif b>a and b>=c:
-    print(b is largest number)
-else:
-    print(c is largest number)"""
-
-"""#find the largest number
+#find the largest number
 a=30
 b=10
 c=20
@@ -186,20 +80,7 @@ elif b>c and b>a:
 else:
    print("c is the largest number")
 
-#check grade
-marks=int(input("enter marks:"))
-if marks>=90:
-  print("grade A")
-if mark>=75:
-  print("grade B")
-if  marks>=50:
-  print("grade c")
-  if marks<50:
-    print("grade D")
-else:
-    print("fail")"""
-
-"""#day of week
+#day of week
 day=int(input("enter a number"))
 if day==1:
   print("monday")
@@ -240,16 +121,9 @@ if num>2:
 elif num<0:
     print("the number negative")
 else:
-    print("zero")"""
+    print("zero")
 
-"""
-
-elif student_score<60:
-    print("fail")"""
-
-
-
-"""#leetcode
+#leetcode
 class Solution(object):
     def isPerfectSquare(self, num):
         if num<2:
@@ -279,13 +153,6 @@ class Solution(object):
             return True
         else:
             return False
-
-#check even or odd 
-num = int(input("Enter the number: "))
-if num % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
 
 #find the largest of two numbers
 a = int(input("Enter first number: "))
@@ -403,7 +270,7 @@ if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
 else:
     print("Not a Leap Year")
 #grade calculations
-marks = int(input("Enter marks: "))"""
+marks = int(input("Enter marks: "))
 
 #check leap year
 year = int(input("Enter a year: "))
@@ -412,11 +279,14 @@ if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
 else:
     print("Not a Leap Year")
 #grade calculations
-marks = int(input("Enter marks: "))
+marks = int(input("Enter marks: "))"""
 
-
-
-
+#take a number as input and check whether it is positive or negative.
+num=int(input("enter the number: "))
+if num>0:
+    print("the number is positive ")
+else:
+    print('the number is negative')
 
 
 
