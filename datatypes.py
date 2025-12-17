0dd6dd2c75952b2cbffcf6d99cac914808f24c25
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-12-17 15:40:43 +0530

    Addedfile

---
+++ datatypes.py
@@ -0,0 +1,26 @@
+#1.Take an integer input and print its *type*.
+a=45
+b=4.0
+c="sahasra"
+d=("sahithi","ujwala","sonu","ashritha")
+e=["sam", "sakshi","swaa"]
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+
+"""#2.Take a float input and print its *double value*.
+a=3.0
+print(a * 2)
+
+num=float(input("enter the value: "))
+print(num * 2)
+
+#3.Store your *name* in a variable and print its *data type*.
+x="SHASRA REDDY PERATI"
+print(type(x))
+
+x=str(input("enter the name: "))
+print(type(x))"""
+
