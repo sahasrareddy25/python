59dae7a9447dbb843aec8c2ffb8f91ff84e90867
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-12-22 10:08:46 +0530

    addedfile

--- list.py
+++ list.py
@@ -1,6 +1,11 @@
 """thislist=["mallareddy"]
-print(thislist)"""
+print(thislist)
 
 #duplicate values
 list=["a","a","b","c","c"]
-print(list)
+print(list)"""
+
+#removed specified item
+thislist=["1","3","3","4","4"]
+thislist.remove("1")
+print(thislist)
