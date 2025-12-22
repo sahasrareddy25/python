3219dc3b59ccfee3488893a6dfd0664bf02db4b3
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-12-22 21:17:02 +0530

    Addedfile

--- list.py
+++ list.py
@@ -8,11 +8,16 @@ print(list)
 #removed specified item using remove() functionc
 thislist=["1","3","3","4","4"]
 thislist.remove("1")
-print(thislist)"""
+print(thislist)
 
 thislist=["sahasra","rishitha","ashritha","aravind"]
 thislist.pop(1)
-print(thislist)
+print(thislist)"""
 
+#use square brackets [] to create a list directly.
+a={"ujwala","arvind","ashritha"}
+b=["sam","jatayu","Ai"]
+c=[1,2,3]
+print(a,b,c)
 
 
