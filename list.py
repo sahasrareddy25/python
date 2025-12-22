b4db0d17ef2ef9cb70a3617cc74eede037c25f87
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-12-22 10:20:05 +0530

    Addedfile

--- list.py
+++ list.py
@@ -3,9 +3,16 @@ print(thislist)
 
 #duplicate values
 list=["a","a","b","c","c"]
-print(list)"""
+print(list)
 
-#removed specified item
+#removed specified item using remove() functionc
 thislist=["1","3","3","4","4"]
 thislist.remove("1")
+print(thislist)"""
+
+thislist=["sahasra","rishitha","ashritha","aravind"]
+thislist.pop(1)
 print(thislist)
+
+
+
