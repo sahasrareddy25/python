1018967e1d8dc86808e8108a38d4f69faa4a6134
sahasrareddy25 <sahasrareddy950@gmail.com>
2025-12-24 20:55:36 +0530

    Addedfile

--- list.py
+++ list.py
@@ -87,10 +87,7 @@ t=(5,15,25,35)
 char_list=list(t)
 print(char_list)
 
-#12.Take a string input from the user and convert it into a list.
-string_input="saashra"
-char_list=list(string_input)
-print(char_list)
+#
 
 
 
@@ -101,13 +98,17 @@ space_count=0
 if char_list in char_list:
     if char_list=" ":
       space_count+=1
-      print("number of spaces:", space_count)"""
+      print("number of spaces:", space_count)
 
 #13.Convert a list [2, 4, 6] into another list using list() and print both.
 text=[1,23,4,45,4]
 char_list=list(text)
 print(text)
 print(char_list)
-
+"""
+#12.Take a string input from the user and convert it into a list.
+string_input="saashra"
+char_list=list(string_input)
+print(char_list)
 
 
