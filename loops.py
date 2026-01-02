4c1c0c1b6d022661271217af65cd675fbacf6a1a
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-01-02 23:24:21 +0530

    Addedfile

--- loops.py
+++ loops.py
@@ -146,9 +146,14 @@ marks = [60, 70, 80]
 for m in marks:
     print(m)"""
 
-#Print numbers from 10 to 1
+"""#Print numbers from 10 to 1
 for i in range(10,0,-1):
-    print(i)
+    print(i)"""
+
+#Print even numbers from 1 to 20
+for i in range(1,21):
+    if i%2==0:
+        print(i)
 
 
 
