7ea45ce5e9c7320901ce1689a4304c197f18e274
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-01-11 20:38:31 +0530

    Addedfile

--- practice.py
+++ practice.py
@@ -1,4 +1,8 @@
-#reverse string
+"""#reverse string
 word="Hello Iam Sahasra"
 words=word.split()
-print(" ".join(words[::-1]))
+print(" ".join(words[::-1]))"""
+
+#Count frequency of words
+sentence="hello Sashra, currentlu working on the most trending ascept of the technology to gain knowledge"
+print(len(sentence.split()))
