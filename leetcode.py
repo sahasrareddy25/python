aa53620a2304004f9858780f20b7d41b6fb8bcbb
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-01-13 21:37:14 +0530

    Addedfile

--- leetcode.py
+++ leetcode.py
@@ -113,7 +113,7 @@ class Solution:
                 result += str(count) + s[i]
                 i += 1
             s = result
-        return s"""
+        return s
 
 #two sum
 class solution(object):
@@ -121,7 +121,35 @@ class solution(object):
         for i in range(len(nums)):
             for j in range(i+1,len(nums)):
                 if nums[i]+nums[j]==target:
-                    return[i,j]
+                    return[i,j]"""
+
+#Number of islands
+class Solution:
+    def numIslands(self, grid):
+        if not grid:
+            return 0
+        
+        rows = len(grid)
+        cols = len(grid[0])
+        count = 0
+
+        def dfs(r, c):
+            if r < 0 or c < 0 or r >= rows or c >= cols or grid[r][c] == "0":
+                return
+            grid[r][c] = "0"  # mark visited
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1":
+                    count += 1
+                    dfs(r, c)
+        
+        return count
+
 
 
 
