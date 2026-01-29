081ab665c65512925dbc6752954a55a042b2fe70
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-01-29 14:51:03 +0530

    Addedfile

--- basic.py
+++ basic.py
@@ -3,19 +3,19 @@
 sname="sahasra"
 age=18
 gender="female"
-print(sname,age,gender)"""
+print(sname,age,gender)
 
 #1.taking the multiple inputs
 #Take two integers as input in a single line and print their sum.
 x,y=input("enter the values: ")
 print("number of male students: ",x)
-print("number of female students: ",y)
+print("number of female students: ",y)"""
 
-"""#2.Take three integers in one line and print the maximum number.
+#2.Take three integers in one line and print the maximum number.
 x,y,z=input("enter the values: ")
 print("enter the first values: ", x)
 print("enter the second value:", y)
-print("enter the third value: ", z)"""
+print("enter the third value: ", z)
 
 
 
