cccf4346e9e107e4a96e2fc1e38686636f2f1fc9
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-01-30 17:51:27 +0530

    Addedfile

--- basic.py
+++ basic.py
@@ -18,20 +18,20 @@ print("enter the second value:", y)
 print("enter the third value: ", z)"""
 
 
-#1.reverse string
+"""#1.reverse string
 word="Hello Iam Sahasra"
 words=word.split()
 print(" ".join(words[::-1]))
 
-"""#2.Count frequency of words
+#2.Count frequency of words
 sentence="hello Sashra, currentlu working on the most trending ascept of the technology to gain knowledge"
-print(len(sentence.split()))
+print(len(sentence.split()))"""
  
 #if counting characters
 sentence="hello Sashra, currentlu working on the most trending ascept of the technology to gain knowledge"
 print(len(sentence))
 
-#3.Remove Duplicates for list
+"""#3.Remove Duplicates for list
 arr=[1,3,5,3,1,4,5,5,7,9]
 result=list(set(arr))
 print(result)
