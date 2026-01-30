ac6cdfd1f7469928f42c9092cc8be272767a39d0
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-01-30 17:52:07 +0530

    addedfile

--- basic.py
+++ basic.py
@@ -25,18 +25,18 @@ print(" ".join(words[::-1]))
 
 #2.Count frequency of words
 sentence="hello Sashra, currentlu working on the most trending ascept of the technology to gain knowledge"
-print(len(sentence.split()))"""
+print(len(sentence.split()))
  
 #if counting characters
 sentence="hello Sashra, currentlu working on the most trending ascept of the technology to gain knowledge"
-print(len(sentence))
+print(len(sentence))"""
 
-"""#3.Remove Duplicates for list
+#3.Remove Duplicates for list
 arr=[1,3,5,3,1,4,5,5,7,9]
 result=list(set(arr))
 print(result)
 
-#4.Take two numbers as input and print their sum
+"""#4.Take two numbers as input and print their sum
 a=int(input("enter the first number: "))
 b=int(input("enter the second number: "))
 print("sum", a+b)
