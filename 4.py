7a84fa525e82a758d0d1a23cad9870c0c9fff2f8
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-02-04 23:40:36 +0530

    Addedfile

---
+++ 4.py
@@ -0,0 +1,14 @@
+"""Take a string and print its length.
+Take a string and print the first and last character.
+Take a string and check if it is empty or not.
+Take a name and print "Hello, <name>!".
+Check if a string contains the word "python".
+Take a string and check if it is a palindrome.
+Count the number of vowels in a string.
+Take two strings and check if they are equal.
+Convert a string to uppercase.
+Take a string and print it reversed."""
+
+#1.Take a string and print its length.
+str="sahasra"
+print(len(str))
