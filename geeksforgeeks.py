7efa3321959983ffbcd24dbb66ac3aafc79098a6
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-02-15 17:01:57 +0530

    Addedfile

--- geeksforgeeks.py
+++ geeksforgeeks.py
@@ -1,4 +1,4 @@
-#The Painter's Partition Problem-II
+"""#The Painter's Partition Problem-II
 #Difficulty: HardAccuracy: 27.52%Submissions: 172K+Points: 8
 #Given an array arr[] where each element denotes the length of a board, and an integer k representing the number of painters available. Each painter takes 1 unit of time to paint 1 unit length of a board.
 #Determine the minimum amount of time required to paint all the boards, under the constraint that each painter can paint only a contiguous sequence of boards (no skipping or splitting allowed).
@@ -27,4 +27,19 @@ class Solution:
                 high = mid - 1
             else:
                 low = mid + 1
-        return answer
+        return answer"""
+
+#Chocolate Distribution Problem
+#Given an array arr[] of positive integers, where each value represents the number of chocolates in a packet. Each packet can have a variable number of chocolates. There are m students, the task is to distribute chocolate packets among m students such that -
+#i. Each student gets exactly one packet.
+#ii. The difference between maximum number of chocolates given to a student and minimum number of chocolates given to a student is minimum and return that minimum possible difference.
+class Solution:
+    def findMinDiff(self, arr, m):
+        n = len(arr)
+        if m > n:
+            return -1
+        arr.sort()
+        min_diff = float('inf')
+        for i in range(n - m + 1):
+            min_diff = min(min_diff, arr[i + m - 1] - arr[i])
+            return min_diff
