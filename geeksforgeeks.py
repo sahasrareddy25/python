0d48cd8e0831a16ee000e666e633fded4cecd1f8
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-02-18 23:42:51 +0530

    Addedfile

--- geeksforgeeks.py
+++ geeksforgeeks.py
@@ -53,7 +53,7 @@ class Solution:
         for i in range(1, len(arr)):
             if arr[i][0] < arr[i-1][1]:
                 return False
-        return True"""
+        return True
 
 
 #Maximum number of overlapping Intervals
@@ -79,5 +79,57 @@ class Solution:
             else:
                 curr -= 1
                 j += 1
-        return maxi
+        return maxi """
+
+
+#Count Inversions
+#Given an array of integers arr[]. You have to find the Inversion Count of the array. 
+#Note: Inversion count is the number of pairs of elements (i, j) such that i < j and arr[i] > arr[j].
+class Solution:
+    def inversionCount(self, arr):
+
+        def merge_sort(arr, temp, left, right):
+            inv_count = 0
+            if left < right:
+                mid = (left + right) // 2
+
+                inv_count += merge_sort(arr, temp, left, mid)
+                inv_count += merge_sort(arr, temp, mid + 1, right)
+                inv_count += merge(arr, temp, left, mid, right)
+
+            return inv_count
+
+        def merge(arr, temp, left, mid, right):
+            i = left
+            j = mid + 1
+            k = left
+            inv_count = 0
+
+            while i <= mid and j <= right:
+                if arr[i] <= arr[j]:
+                    temp[k] = arr[i]
+                    i += 1
+                else:
+                    temp[k] = arr[j]
+                    inv_count += (mid - i + 1)
+                    j += 1
+                k += 1
+
+            while i <= mid:
+                temp[k] = arr[i]
+                i += 1
+                k += 1
+
+            while j <= right:
+                temp[k] = arr[j]
+                j += 1
+                k += 1
+
+            for idx in range(left, right + 1):
+                arr[idx] = temp[idx]
+
+            return inv_count
+
+        temp = [0] * len(arr)
+        return merge_sort(arr, temp, 0, len(arr) - 1)
 
