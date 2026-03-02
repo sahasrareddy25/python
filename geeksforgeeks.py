5a7b6bd80e728b9fe86c495fda721803b855be44
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-03-02 23:35:16 +0530

    Addedfile

--- geeksforgeeks.py
+++ geeksforgeeks.py
@@ -336,7 +336,7 @@ arr2 = [10, 20, 30, 40]
 x = 32
 
 print(closestPair(arr1, arr2, x))
-"""
+
 
 #Move All Zeroes to End
 class Solution:
@@ -347,4 +347,41 @@ class Solution:
         for i in range(n):
             if arr[i] != 0:
                 arr[j], arr[i] = arr[i], arr[j]
-                j += 1
+                j += 1"""
+
+#Trapping Rain Water
+#Given an array arr[] with non-negative integers representing the height of blocks. If the width of each block is 1, compute how much water can be trapped between the blocks during the rainy season.
+class Solution:
+    def maxWater(self, arr):
+        n = len(arr)
+        
+        left = 0
+        right = n - 1
+        
+        left_max = 0
+        right_max = 0
+        
+        water = 0
+        
+        while left <= right:
+            
+            if arr[left] <= arr[right]:
+                
+                if arr[left] >= left_max:
+                    left_max = arr[left]
+                else:
+                    water += left_max - arr[left]
+                    
+                left += 1
+            
+            else:
+                
+                if arr[right] >= right_max:
+                    right_max = arr[right]
+                else:
+                    water += right_max - arr[right]
+                    
+                right -= 1
+        
+        return water
+        
