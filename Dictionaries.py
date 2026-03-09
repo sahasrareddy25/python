8b44ecdc0fd01d6c59bd0f2f1baab88edf429cbc
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-03-09 18:25:41 +0530

    Addedfile

---
+++ Dictionaries.py
@@ -0,0 +1,9 @@
+#In data structures stroing the data in key value pair 
+
+#1.store the student details and print name of the student
+student={
+    "name": "Saashra",
+    "age": "20",
+    "branch": "cse"
+}
+print(student['name'])
