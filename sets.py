633a3d7a401780802cafb23faf58463b590e4784
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-03-15 23:55:08 +0530

    Addedfile

--- sets.py
+++ sets.py
@@ -1,3 +1,8 @@
-#creating a set 
+"""#1.creating a set 
 nums={1,2,3,4,5,5,6,7}
-print(nums)     #here it remove the duplicate values
+print(nums)     #here it remove the duplicate values"""
+
+#2.addding a element to a set
+nums={1,2,3,4}
+nums.add(25)
+print(nums)
