4a5dab67eec2e604e5ce094f057d6d7aab417e4d
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-03-15 23:58:00 +0530

    Addedfile

--- sets.py
+++ sets.py
@@ -5,9 +5,15 @@ print(nums)     #here it remove the duplicate values
 #2.addding a element to a set
 nums={1,2,3,4}
 nums.add(25)
-print(nums)"""
+print(nums)
 
 #3.removing an element
 nums={1,2,3,4,5,6}
 nums.remove(6)
-print(nums)
+print(nums)"""
+
+#4.union of two sets
+a={1,2,3,4}
+b={5,6,7,8,9}
+res=a.union(b)
+print(res)
