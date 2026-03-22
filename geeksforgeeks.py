49e442d85d4cd2751b2144cd6448d2c394d87ccb
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-03-23 00:00:51 +0530

    Addedfile

--- geeksforgeeks.py
+++ geeksforgeeks.py
@@ -541,3 +541,21 @@ class Solution:
             time += 1
         
         return time if fresh == 0 else -1
+
+
+#
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        left = 0
+        current_sum = 0
+        min_len = float('inf')
+        
+        for right in range(len(nums)):
+            current_sum += nums[right]
+            
+            while current_sum >= target:
+                min_len = min(min_len, right - left + 1)
+                current_sum -= nums[left]
+                left += 1
+        
+        return 0 if min_len == float('inf') else min_len
