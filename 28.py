77dc602a3d516aa85da34bdcaf104ffaf97ff469
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-03-28 23:46:15 +0530

    Addedfile

---
+++ 28.py
@@ -0,0 +1,12 @@
+#find out the largest of 3Numbers
+a=int(input("enter the first number: "))
+b=int(input("enter the first number: "))
+c=int(input("enter the first number: "))
+largest=a
+if b>largest:
+    print(b is largest)
+elif c>largest:
+    print(c is largest)
+else:
+    print(a is largest)
+    
