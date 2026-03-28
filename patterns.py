943dfb64b3f94a101e6ed74c1b391ec9688f4092
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-03-29 00:31:46 +0530

    Addedfile

--- patterns.py
+++ patterns.py
@@ -1,8 +1,19 @@
 #pattern is nothing but printing the different shapes *
-#*
-#**
-#***
-#****
+"""*
+**
+***
+****"""
 n=4
 for i in range(1, n+1):
    print("*" * i)
+
+"""
+*
+**
+***
+****
+*****"""
+n=4
+for i in range(1, n+1):
+    print(" " *(n-1)+ "*" * (2*i-1))
+
