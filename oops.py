1ceefc5b92f8c7ad24e59924a64d0f5ba7f35d8f
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-03-31 23:50:12 +0530

    Addedfile

--- oops.py
+++ oops.py
@@ -1,4 +1,4 @@
-#parent class- Employee-method, child class developer-method- create obj for developer class 
+"""#parent class- Employee-method, child class developer-method- create obj for developer class 
 #with the help of it access the parent class method
 class Employee:
     def show_employee(self):
@@ -6,6 +6,18 @@ class Employee:
 class Developer(Employee):
     def show_developer(self):
         print("Developer class method")
-dev=Developer()
-dev.show_developer()
-dev.show_employee()
+d=Developer()
+d.show_developer()
+d.show_employee()"""
+
+
+#parent method in child.
+class animal:
+    def sound(Self):
+        print("animal make sounds")
+
+class dog(animal):
+    def sound(self):
+        print("dog barks")
+d=dog()
+d.sound()
