4675d49fdf6d37dde3f1061247126b66ddce2276
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-04-03 00:44:14 +0530

    addedfile

--- functions.py
+++ functions.py
@@ -1,2 +1,9 @@
-def my_function():
+"""#creating a function
+def my_function():         #def is a key word  
   print("Hellooo func")
+"""
+#calling a func
+def my_function():
+  print("Hello func")
+my_function()
+
