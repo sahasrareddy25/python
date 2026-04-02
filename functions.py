d242805be2fa4554e3234d2580d1f4486865c23b
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-04-03 00:45:21 +0530

    Addedfile

--- functions.py
+++ functions.py
@@ -1,9 +1,15 @@
 """#creating a function
 def my_function():         #def is a key word  
   print("Hellooo func")
-"""
+
 #calling a func
 def my_function():
   print("Hello func")
 my_function()
-
+"""
+#a func can call multiple times
+def my_function():
+  print("Hello func")
+my_function()
+my_function()
+my_function()
