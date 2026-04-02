c3296f84f8597bd92f5e19c1358fd91ab2751688
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-04-03 00:52:46 +0530

    Addedfile

--- functions.py
+++ functions.py
@@ -12,12 +12,22 @@ def my_function():
   print("Hello func")
 my_function()
 my_function()            #multiple times
-my_function()"""
+my_function()
 
 #converting fahrenheit to celsius
 def fahrenheit_to_celsius(fahrenheit):
   return (fahrenheit-32)*5/9
 print(fahrenheit_to_celsius(77))
 print(fahrenheit_to_celsius(95))
-print(fahrenheit_to_celsius(50))
+print(fahrenheit_to_celsius(50))"""
+
+#evenodd 
+def evenOdd(x):
+    if (x%2==0):
+        return "Even"
+    else:
+        return "Odd"
+print(evenOdd(16))
+print(evenOdd(7))
+
 
