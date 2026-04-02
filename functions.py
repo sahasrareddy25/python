e928c57aad190479a9598ea44f80fc980e705071
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-04-03 00:49:57 +0530

    Addedfile

--- functions.py
+++ functions.py
@@ -3,13 +3,21 @@ def my_function():         #def is a key word
   print("Hellooo func")
 
 #calling a func
+def my_function():  
+  print("Hello func")
+my_function()             #single time
+
+#we can call same func multiple times
 def my_function():
   print("Hello func")
 my_function()
-"""
-#a func can call multiple times
-def my_function():
-  print("Hello func")
-my_function()
-my_function()
-my_function()
+my_function()            #multiple times
+my_function()"""
+
+#converting fahrenheit to celsius
+def fahrenheit_to_celsius(fahrenheit):
+  return (fahrenheit-32)*5/9
+print(fahrenheit_to_celsius(77))
+print(fahrenheit_to_celsius(95))
+print(fahrenheit_to_celsius(50))
+
