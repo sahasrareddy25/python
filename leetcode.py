b621fc3b2fdc4eb9da8ef76f1949fe1d50e8c891
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-04-25 23:36:45 +0530

    Addedfile

--- leetcode.py
+++ leetcode.py
@@ -790,7 +790,7 @@ class Solution(object):
        map={')':'(','}':'{',']',:'['}
        stack=[]
        for char in s:
-        if char in map:"""
+        if char in map:
 
 #sort colors
 class Solution:
@@ -806,4 +806,17 @@ class Solution:
             else:
                 nums[mid], nums[high] = nums[high], nums[mid]
                 high -= 1
-           
+           """
+
+#check if the grids satisfies conditions
+class Solution:
+    def satisfiesConditions(self, grid):
+        m, n = len(grid), len(grid[0])
+        for i in range(m):
+            for j in range(n):
+                if i < m - 1 and grid[i][j] != grid[i+1][j]:
+                    return False
+                if j < n - 1 and grid[i][j] == grid[i][j+1]:
+                    return False
+        
+        return True
