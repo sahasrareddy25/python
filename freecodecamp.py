20f5053192106410bd9f878f26d0b2ae24674932
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-05-12 17:06:35 +0530

    Addedfile

--- freecodecamp.py
+++ freecodecamp.py
@@ -95,7 +95,7 @@ You should print True only if the person has a bike and it is not raining.
 Otherwise, you should print False.
 If the distance is greater than 6 miles:
 You should print True if the person has a car or has a ride-share app.
-Otherwise, you should print False."""
+Otherwise, you should print False.
 
 distance_mi = 5
 is_raining = True
@@ -122,4 +122,33 @@ else:
     if has_car or has_ride_share_app:
         print(True)
     else:
-        print(False)
+        print(False)"""
+
+"""build an Apply Discount Function
+In this lab you will write a function that calculates the final price of an item after applying a percentage discount.
+For example, if the price of an item is 50 and a discount of 20 is applied, the discount amount is 10, and the final price is 40.
+Objective: Fulfill the user stories below to complete the lab.
+User Stories:
+You should define a function named apply_discount.
+The apply_discount function should take exactly two parameters: price and discount.
+If price is not a number (int or float), the function should return the string The price should be a number.
+If discount is not a number (int or float), the function should return the string The discount should be a number.
+If price is less than or equal to 0, the function should return the string The price should be greater than 0.
+If discount is less than 0 or greater than 100, the function should return the string The discount should be between 0 and 100.
+If both inputs are valid, the function should calculate the discount as a percentage of the price.
+The function should return the final price after applying the discount.
+"""
+def apply_discount(price, discount):
+    if not isinstance(price, (int, float)):
+        return "The price should be a number"
+    if not isinstance(discount, (int, float)):
+        return "The discount should be a number"
+    if price <= 0:
+        return "The price should be greater than 0"
+    if discount < 0 or discount > 100:
+        return "The discount should be between 0 and 100"
+    discount_amount = (price * discount) / 100
+    final_price = price - discount_amount
+    return final_price
+print(apply_discount(50, 20))
+
