2f7d938a381ec4d8ccfaba03eec2be6c516ad0f4
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-05-20 12:53:37 +0530

    Addedfile

--- revision.py
+++ revision.py
@@ -80,7 +80,7 @@ def palindrome(text):
         return "palindrome"
     else:
         return "not palindrome"
-    print(palindrome("madam"))"""
+    print(palindrome("madam"))
 
 #counting vowels in a string
 def vowels(text):
@@ -90,3 +90,16 @@ def vowels(text):
             count+=1
     return count
 print(vowels("session"))
+"""
+#ugly number
+class Solution(object):
+    def isUgly(self, n):
+        if n<=0:
+            return False
+        while n%2==0:
+            n=n//2
+        while n%3==0:
+            n=n//3
+        while n%5==0:
+            n=n//5
+        return n==1
