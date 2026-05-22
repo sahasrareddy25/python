d7978b824af5db8be65c28138a21336c4fe08f70
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-05-22 22:41:10 +0530

    Addedfile

--- operators.py
+++ operators.py
@@ -119,7 +119,6 @@ class Solution:
             s.add(int(num))
         return len(s)
 
-"""
 #count subarrays of length three with a condition
 class Solution(object):
     def countSubarrays(self, nums):
@@ -128,4 +127,14 @@ class Solution(object):
         for i in range(n-2):
             if 2*(nums[i]+nums[i+2])==nums[i+1]:
                 count += 1
-        return count
+        return count"""
+
+#maximum element in list
+def max(nums):
+    largest=nums[0]
+    for i in nums:
+        if i>largest:
+            largest=i
+    return largest
+print(max([10,20,49]))
+"""
