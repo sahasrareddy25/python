a9ca2509891b2c8675a63c9f247902d2da24593e
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-06-02 22:05:16 +0530

    addedfile

--- hackerrank.py
+++ hackerrank.py
@@ -1,9 +1,20 @@
-#longest common prefix
+"""#longest common prefix
 class Solution(object):
     def longestCommonPrefix(self, strs):
         p = strs[0]
         for s in strs[1:]:
             while not s.startswith(p):
                 p = p[:-1]
-        return p
+        return p"""
+
+#count the number of special characters
+class Solution(object):
+    def numberOfSpecialChars(self, word):
+        s = set(word)
+        count = 0
+        for ch in "abcdefghijklmnopqrstuvwxyz":
+            if ch in s and ch.upper() in s:
+                count += 1
+        return count
+        
         
