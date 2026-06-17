534a71d888fa13dc5dd87fe89846d3ebe0f240b9
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-06-17 22:10:59 +0530

    Addedfile

--- leetcode2.py
+++ leetcode2.py
@@ -161,9 +161,33 @@ SELECT
 FROM Person p
 LEFT JOIN Address a
 ON p.personId = a.personId;
-"""
+
 #delete duplicate emails
 DELETE p1
-FROM Person p1, Person p2
+FROM Person p1, Person 
 WHERE p1.email = p2.email
   AND p1.id > p2.id;
+  """
+#isomorphic string
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        s_to_t = {}
+        t_to_s = {}
+
+        for i in range(len(s)):
+            c1 = s[i]
+            c2 = t[i]
+
+            if c1 in s_to_t:
+                if s_to_t[c1] != c2:
+                    return False
+            else:
+                s_to_t[c1] = c2
+
+            if c2 in t_to_s:
+                if t_to_s[c2] != c1:
+                    return False
+            else:
+                t_to_s[c2] = c1
+
+        return True
