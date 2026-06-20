72d031704573d714c4a1ebd6f00c7929fe102e78
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-06-20 14:09:10 +0530

    Addedfile

--- leetcode2.py
+++ leetcode2.py
@@ -291,7 +291,7 @@ class Solution:
         if ans < len(s):
             ans += 1
 
-        return ans"""
+        return ans
 #repeated sub string problem
 class Solution:
     def repeatedSubstringPattern(self, s):
@@ -304,4 +304,33 @@ class Solution:
                     return True
 
         return False
-        
+        """
+
+
+class Solution:
+    def maxBuilding(self, n, restrictions):
+        restrictions.append([1, 0])
+        restrictions.sort()
+        if restrictions[-1][0] != n:
+            restrictions.append([n, n - 1])
+        m = len(restrictions)
+        for i in range(1, m):
+            d = restrictions[i][0] - restrictions[i - 1][0]
+            restrictions[i][1] = min(
+                restrictions[i][1],
+                restrictions[i - 1][1] + d
+            )
+        for i in range(m - 2, -1, -1):
+            d = restrictions[i + 1][0] - restrictions[i][0]
+            restrictions[i][1] = min(
+                restrictions[i][1],
+                restrictions[i + 1][1] + d
+            )
+        ans = 0
+        for i in range(1, m):
+            x1, h1 = restrictions[i - 1]
+            x2, h2 = restrictions[i]
+            d = x2 - x1
+            peak = (h1 + h2 + d) // 2
+            ans = max(ans, peak)
+        return ans
