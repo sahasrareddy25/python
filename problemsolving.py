bcf7c01fb6ef0ebe2420c5549ebe76b35d05077d
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-06-29 11:45:53 +0530

    Addedfile

--- problemsolving.py
+++ problemsolving.py
@@ -1,4 +1,4 @@
-#combination sum II
+"""#combination sum II
 class Solution:
     def combinationSum2(self, candidates, target):
         candidates.sort()
@@ -16,4 +16,22 @@ class Solution:
                 dfs(i + 1, target - candidates[i], path)
                 path.pop()
         dfs(0, target, [])
-        return ans
+        return ans"""
+#unique paths II
+class Solution:
+    def uniquePathsWithObstacles(self, obstacleGrid):
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [[0] * n for _ in range(m)]
+        if obstacleGrid[0][0] == 1:
+            return 0
+        dp[0][0] = 1
+        for i in range(m):
+            for j in range(n):
+                if obstacleGrid[i][j] == 1:
+                    dp[i][j] = 0
+                else:
+                    if i > 0:
+                        dp[i][j] += dp[i - 1][j]
+                    if j > 0:
+                        dp[i][j] += dp[i][j - 1]
+        return dp[m - 1][n - 1]
