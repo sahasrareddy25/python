109556e8a182e5d2ef7a3c00ceb67b981bd8496c
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-07-02 10:00:00 +0530

    Solved LeetCode problems - July 2

--- problemsolving2.py
+++ problemsolving2.py
@@ -1,4 +1,4 @@
-#Remove Nth Node From End of List
+"""#Remove Nth Node From End of List
 class Solution:
     def removeNthFromEnd(self, head, n):
         dummy = ListNode(0)
@@ -16,4 +16,23 @@ class Solution:
 
         slow.next = slow.next.next
 
-        return dummy.next
+        return dummy.next"""
+
+#minimum path sum
+class Solution(object):
+    def minPathSum(self, grid):
+        m = len(grid)
+        n = len(grid[0])
+
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    continue
+                elif i == 0:
+                    grid[i][j] += grid[i][j - 1]
+                elif j == 0:
+                    grid[i][j] += grid[i - 1][j]
+                else:
+                    grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
+
+        return grid[m - 1][n - 1]
