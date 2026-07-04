185c2af6fa3ddae68a2774d33886e5a757763f2d
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-07-04 10:00:00 +0530

    Solved LeetCode problems - July 4

--- problemsolving2.py
+++ problemsolving2.py
@@ -35,7 +35,7 @@ class Solution(object):
                 else:
                     grid[i][j] += min(grid[i - 1][j], grid[i][j - 1])
 
-        return grid[m - 1][n - 1]"""
+        return grid[m - 1][n - 1]
 
 #insert internal
 class Solution:
@@ -54,4 +54,28 @@ class Solution:
         while i < n:
             result.append(intervals[i])
             i += 1
-        return result
+        return result"""
+#30. Substring with Concatenation of All Words
+from collections import Counter
+
+class Solution:
+    def findSubstring(self, s, words):
+        if not s or not words:
+            return []
+
+        wl = len(words[0])
+        total = wl * len(words)
+        target = Counter(words)
+        ans = []
+
+        for i in range(len(s) - total + 1):
+            seen = Counter()
+            for j in range(0, total, wl):
+                w = s[i + j:i + j + wl]
+                if seen[w] == target[w]:
+                    break
+                seen[w] += 1
+            else:
+                ans.append(i)
+
+        return ans
