e3ba11f38476a26e36fd9c515c04e1d7297b376f
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-07-09 14:55:52 +0530

    Addedfile

--- problemsolving2.py
+++ problemsolving2.py
@@ -137,7 +137,7 @@ class Solution:
             else:
                 curr = curr.next
 
-        return dummy.next"""
+        return dummy.next
 #Maximum Depth of N-ary Tree
 class Solution:
     def maxDepth(self, root):
@@ -145,4 +145,21 @@ class Solution:
             return 0
         if not root.children:
             return 1
-        return 1 + max(self.maxDepth(child) for child in root.children)
+        return 1 + max(self.maxDepth(child) for child in root.children)"""
+
+#search insert positition
+class Solution(object):
+    def searchInsert(self, nums, target):
+        left, right = 0, len(nums) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return left
