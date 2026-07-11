a044dadc1508a93c58594bcefd77fb283d940350
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-07-11 10:00:00 +0530

    Solved LeetCode problems - July 11

--- code.py
+++ code.py
@@ -12,11 +12,24 @@ class Solution:
             elif res == -1:
                 right = mid - 1
             else:
-                left = mid + 1"""
+                left = mid + 1
 
 #guess the nummber
 DELETE p1
 FROM Person p1
 JOIN Person p2
 ON p1.email = p2.email
-AND p1.id > p2.id;
+AND p1.id > p2.id;"""
+
+#last pivot element
+class Solution:
+    def pivotIndex(self, nums):
+        total = sum(nums)
+        left = 0
+
+        for i in range(len(nums)):
+            if left == total - left - nums[i]:
+                return i
+            left += nums[i]
+
+        return -1
