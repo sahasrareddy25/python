fb2dc419dbe67dc3b90508862d31044116e21071
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-07-12 10:00:00 +0530

    Solved LeetCode problems - July 12

--- code.py
+++ code.py
@@ -19,7 +19,7 @@ DELETE p1
 FROM Person p1
 JOIN Person p2
 ON p1.email = p2.email
-AND p1.id > p2.id;"""
+AND p1.id > p2.id;
 
 #last pivot element
 class Solution:
@@ -32,4 +32,20 @@ class Solution:
                 return i
             left += nums[i]
 
-        return -1
+        return -1"""
+#fizz buzz
+class Solution:
+    def fizzBuzz(self, n):
+        ans = []
+
+        for i in range(1, n + 1):
+            if i % 15 == 0:
+                ans.append("FizzBuzz")
+            elif i % 3 == 0:
+                ans.append("Fizz")
+            elif i % 5 == 0:
+                ans.append("Buzz")
+            else:
+                ans.append(str(i))
+
+        return ans
