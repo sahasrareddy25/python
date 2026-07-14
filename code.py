8aebeae5c6967fa023ac5fabbc377f60e47d36a6
sahasrareddy25 <sahasrareddy950@gmail.com>
2026-07-14 10:00:00 +0530

    Solved LeetCode problems - July 14

--- code.py
+++ code.py
@@ -48,8 +48,20 @@ class Solution:
             else:
                 ans.append(str(i))
 
-        return ans"""
+        return ans
 #student attendance record I
 class Solution:
     def checkRecord(self, s):
-        return s.count('A') < 2 and "LLL" not in s
+        return s.count('A') < 2 and "LLL" not in s"""
+#teoplitz matrix
+class Solution:
+    def isToeplitzMatrix(self, matrix):
+        rows = len(matrix)
+        cols = len(matrix[0])
+
+        for i in range(1, rows):
+            for j in range(1, cols):
+                if matrix[i][j] != matrix[i - 1][j - 1]:
+                    return False
+
+        return True
